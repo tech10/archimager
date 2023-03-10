755e10eef51e017b70f460c12aacb3355438c96b
Blake G <blakeblind@gmail.com>
2023-03-10 14:50:12 -0700

    Add dosfstools to the list of packages needed to build the image.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # This is designed to build an Arch Linux image
 # from Arch Linux itself. It may not work on other distributions.
 # Update all packages, then install the following to build an image:
-# arch-install-scripts syslinux parted gptfdisk
+# arch-install-scripts syslinux parted gptfdisk dosfstools
 # These images are designed to boot on an MBR and EFI.
 # They will work with the serial console by default, which is known to work with Linode, and possibly other VM hosts.
 if [ $EUID -ne 0 ]; then
