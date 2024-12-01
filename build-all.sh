e7731a20d1cf3487dcd911ba6eb91a6f90bd00b4
Blake G <blakeblind@gmail.com>
2024-12-01 07:21:14 -0700

    Create a script for building all images.

---
+++ build-all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+# Build all images.
+. ./common-vars.sh
+img_l="./arch-linode.img.gz"
+img_b="./arch-bios.img.gz"
+img_e="./arch-efi.img.gz"
+img_h="./arch-hybrid.img.gz"
+img_comp="${archimg}.gz"
+logd="./log"
+mkdir $logd
+builder() {
+local script=$1
+local imggz=$2
+local logf="${logd}/$(basename ${script}.log)"
+echo "Running $script and writing all output to $logf"
+$script >$logf 2>&1
+echo Compressing.
+./compress.sh
+echo Moving.
+mv -v ${img_comp} ${imggz}
+echo Removing built image.
+rm -v ${archimg}
+echo Success.
+}
+builder ./build-linode.sh $img_l
+builder ./build-bios.sh $img_b
+builder ./build-efi.sh $img_e
+builder ./build-hybrid.sh $img_h
