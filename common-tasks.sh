9ade24b89d9086f0ea829cec47f2ed3ccf0e6d77
Blake G <blakeblind@gmail.com>
2024-12-03 10:53:00 -0700

    Become a bit more verbose on what task is being performed and when.

--- common-tasks.sh
+++ common-tasks.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Perform on all installs.
+echo "Enabling systemd services."
 check systemctl --root=${imgdir} enable sshd systemd-networkd systemd-resolved systemd-timesyncd
+echo "Copying systemd files."
 check cp -rv ./systemd/* ${imgdir}/etc/systemd/
+echo "Setting resolve.conf"
 check ln -svf /run/systemd/resolve/stub-resolv.conf ${imgdir}/etc/resolv.conf
+echo "Setting time zone: ${tz}"
+check chroot ${imgdir} /usr/bin/ln -svf "/usr/share/zoneinfo/${tz}" /etc/localtime
+echo "Clearing root password."
 check chroot ${imgdir} /usr/bin/passwd -d root
 sshkeys="${HOME}/.ssh/authorized_keys"
 sshdir="${imgdir}/root/.ssh"
@@ -18,7 +24,7 @@ customscript="custom.sh"
 customscriptpath="${imgdir}/root/${customscript}"
 customsvc="custom.service"
 if [ -f "${customscript}" ]; then
-echo "Custom script exists."
+echo "Custom script exists. Copying."
 check cp -v "${customscript}" "${customscriptpath}"
 check chmod -v +x "${customscriptpath}"
 customsystemdsvc >${imgdir}/etc/systemd/system/${customsvc}
