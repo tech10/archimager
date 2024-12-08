896ce9a550ccd0c57b544064f8f7520d13a19abc
Blake G <blakeblind@gmail.com>
2024-12-08 12:33:58 -0700

    Ensure loop device is detached correctly and ensure disk sync before detach.

--- common-cleanup.sh
+++ common-cleanup.sh
@@ -2,7 +2,9 @@
 check sync
 check umount -Rv ${imgdir}
 check rm -rv ${imgdir}
-dev_type=$(lsblk -n -o TYPE "$diskdev" 2>/dev/null)
+dev_type=$(lsblk -n -o TYPE "$diskdev" 2>/dev/null | head -n 1)
 if [[ "$dev_type" == "loop" ]]; then
-check losetup -vd ${diskdev}
+echo Detaching ${diskdev}
+check sync
+check losetup -d ${diskdev}
 fi
