e9e7e2fcd83e2478e9e2c80dc343f81b54601c30
Blake G <blakeblind@gmail.com>
2024-12-12 08:30:58 -0700

    Do not create random seed for systemd-boot EFI install. systemd version >=257 required on the system creating the image.

--- common-boot-efi.sh
+++ common-boot-efi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . ./common-boot-cfg.sh
 # Boot loader install for EFI system only.
-check bootctl --root=${imgdir} --no-variables install
+check bootctl --root=${imgdir} --random-seed=no --no-variables install
 systemdbootcfgldr >${imgdir}/boot/loader/loader.conf
 systemdbootcfgarch / >${imgdir}/boot/loader/entries/arch.conf
 systemdbootcfgarchfallback / >${imgdir}/boot/loader/entries/archfallback.conf
